8cc8df894d5e50d07421bf75629aeb0bebb4c6f5
Alex Guerra <alex.guerra@luizalabs.com>
2018-02-28 10:49:45 -0300

    Add .travis.yml and .ci-docker.sh
    Used for cronjobs and regular builds.

---
+++ .ci-docker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$TRAVIS_TAG" ]; then
+    echo "skip docker imagem generation (no tag detected)"
+    exit 0
+fi
+
+echo "Generate and upload the latest SLUGSTORE image to Docker HUB"
+docker login -u "$DOCKER_USER" -p "$DOCKER_PASS"
+docker build -t "$DOCKER_REGISTRY"/"$DOCKER_IMAGE":"$TRAVIS_TAG" .
+docker push "$DOCKER_REGISTRY"/"$DOCKER_IMAGE":"$TRAVIS_TAG"
+
+exit 0;
